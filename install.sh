6bc043c65cad4479057cdf0c883a8bd1f38897a8
iTo0ns <iitoons@gmail.com>
2017-04-08 20:55:40 +0200

    sd

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 cp -R $HOME/MyDotfiles/ $HOME/.dotfiles/
 
 ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
+ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zlogin
+ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zlogout
 ln -sf $HOME/.dotfiles/urxvt/Xresources $HOME/.Xresources
 
 rm -rf $HOME/MyDotfiles
