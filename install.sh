6f7172d4908d0747488fdf3cad2b55383e96300b
iTo0ns <iitoons@gmail.com>
2017-04-08 20:48:55 +0200

    sq

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp $HOME/MyDotfiles $HOME/.dotfiles
+cp -R $HOME/MyDotfiles/ $HOME/.dotfiles/
 
 ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
 ln -sf $HOME/.dotfiles/urxvt/Xresources $HOME/.Xresources
