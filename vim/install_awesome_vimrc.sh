d1af6404ee47a91a78d3a83caa4199b0ec0b9fb4
Sebastien Martinez <itoons@archbook.localdomain>
2017-04-08 21:57:23 +0200

    jj

---
+++ vim/install_awesome_vimrc.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+cd ~/.vim_runtime
+
+echo 'set runtimepath+=~/.dotfiles/vim
+source ~/.dotfiles/vim/vimrcs/basic.vim
+source ~/.dotfiles/vim/vimrcs/filetypes.vim
+source ~/.dotfiles/vim/vimrcs/plugins_config.vim
+source ~/.dotfiles/vim/vimrcs/extended.vim
+
+try
+source ~/.dotfiles/my_configs.vim
+catch
+endtry' > ~/.vimrc
+
+echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
