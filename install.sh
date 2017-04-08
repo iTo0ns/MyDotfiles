0a704ef87690555b80b98ba51925c6bad2629454
Sebastien Martinez <itoons@archbook.localdomain>
2017-04-08 21:51:24 +0200

    hjk

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-sudo pacman -S --force --noconfirm git git-core ctags ncurses curl wget zip 7zip unrar vim neovim rxvt-unicode zsh
+sudo pacman -S --force --noconfirm git git-core ctags ncurses curl wget zip p7zip unrar vim neovim rxvt-unicode zsh
 
 chsh $USER -s /bin/zsh
 
 cp -R $HOME/MyDotfiles/ $HOME/.dotfiles/
+
+git clone https://github.com/amix/vimrc.git ~/.dotfiles/vim
+sh ~/.dotfiles/vim/install_awesome_vimrc.sh
 
 ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
 ln -sf $HOME/.dotfiles/zsh/zlogin $HOME/.zlogin
 ln -sf $HOME/.dotfiles/zsh/zlogout $HOME/.zlogout
 ln -sf $HOME/.dotfiles/urxvt/Xresources $HOME/.Xresources
 
-mkdir $HOME/nvim/
 
-mv $HOME/.dotfiles/nvim/init.vim $HOME/nvim/init.vim
 
-vim +PlugInstall +qall
