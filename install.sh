c7de5214c416790096aaccb746b7537b92ca9587
iTo0ns <iitoons@gmail.com>
2017-04-08 20:43:06 +0200

    updating

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-mv $HOMEMyDotfiles $HOME.dotfiles
+cp $HOMEMyDotfiles $HOME.dotfiles
 
 ln -sf $HOME.dotfiles/zsh/zshrc $HOME.zshrc
 ln -sf $HOME.dotfiles/urxvt/Xresources $HOME.Xresources
+
+rm -rf $HOMEMyDotfiles
