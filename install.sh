1cac7284f7ff1f97c4ac7e3773c0c8514f361881
Sebastien Martinez <itoons@archbook.localdomain>
2017-04-08 21:59:52 +0200

    kk

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ chsh $USER -s /bin/zsh
 
 cp -R $HOME/MyDotfiles/ $HOME/.dotfiles/
 
-git clone https://github.com/amix/vimrc.git ~/.dotfiles/vim
 sh ~/.dotfiles/vim/install_awesome_vimrc.sh
 
 ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
