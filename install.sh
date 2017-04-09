fe0ca738791cadf8e11a3ffc689189168d59bd96
Sebastien Martinez <itoons@archbook.localdomain>
2017-04-09 11:18:50 +0200

    jk

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ cp -R $HOME/MyDotfiles/ $HOME/.dotfiles/
 
 sh ~/.dotfiles/vim/install_awesome_vimrc.sh
 
+mv $HOME/.atom $HOME/.dotfiles/atom
+
 ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
 ln -sf $HOME/.dotfiles/zsh/zlogin $HOME/.zlogin
 ln -sf $HOME/.dotfiles/zsh/zlogout $HOME/.zlogout
 ln -sf $HOME/.dotfiles/urxvt/Xresources $HOME/.Xresources
 ln -sf $HOME/.dotfiles/atom $HOME/.atom
-exit
+
